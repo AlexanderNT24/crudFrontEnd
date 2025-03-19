ee7fd7c80d89c35acf47d005fcda7f0c0720140e
Alexander Nureña <alexander26nt@gmail.com>
2025-03-19 03:17:15 -0500

    add search

--- js/script.js
+++ js/script.js
@@ -40,3 +40,20 @@ $(document).ready(() => {
     );
   });
 });
+$(document).ready(function() {
+  $('#searchInput').on('keyup', function() {
+      var value = $(this).val().toLowerCase();
+      console.log(value)
+      $('#personTable tbody tr').filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  });
+});
+$(document).ready(function() {
+  $('#searchInput').on('keyup', function() {
+      var value = $(this).val().toLowerCase();
+      $('#personTable tbody tr').filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  });
+});
